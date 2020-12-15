64e332bb50fb6101628602a201b705e346a4a902
Daniel Passos <laurodanielpassos@gmail.com>
2020-12-15 00:53:47 -0300

    add events to both buttons

--- js-for-beginners/counter/script.js
+++ js-for-beginners/counter/script.js
@@ -1,29 +1,17 @@
-const block = document.querySelector('blockquote');
-const cita = document.querySelector('cite');
-const btn = document.querySelector('button')
+const contador = document.querySelector('span');
+const low = document.querySelector('.low');
+const add = document.querySelector('.add');
 
-const falas = [
-  {
-    fala: "O que realmente deixa um homem lisonjeado é o fato de você o considerar digno de adulação.",
-    autor: "Bernard Shaw"
-  },
-  {
-    fala:"Há livros escritos para evitar espaços vazios na estante.",
-    autor:"Carlos Drummond de Andrade"
-  },
-  {
-    fala:"Engolimos de uma vez a mentira que nos adula e bebemos gota a gota a verdade que nos amarga",
-    autor: "Denis Diderot"
-  },
-  {
-    fala: "A mentira é o único privilégio do homem sobre todos os outros animais.",
-    autor: "Fiódor Dostoiévski"
-  }
-]
+let counter = 0;
 
-function changeQuote(){
-  const mudaFala = parseInt(Math.random()*falas.length);
-  block.innerText = falas[mudaFala].fala
-  cita.innerText = falas[mudaFala].autor
+function minus(){
+   counter--;
+  contador.textContent = counter;
 }
-btn.addEventListener('click', changeQuote);
+
+function more(){
+  counter++;
+ contador.textContent = counter;
+}
+low.addEventListener('click', minus);
+add.addEventListener('click', more);
