be572477fbd8cab35c2e79c725f5dd931ee4352f
Daniel Passos <laurodanielpassos@gmail.com>
2020-12-26 22:52:48 -0300

    Add Day 1 js

---
+++ 30 days of javascript/script.js
@@ -0,0 +1,45 @@
+// Exercise 1
+
+// comments can make code readable
+
+// Exercise 2
+
+// Welcome to 30DaysOfJavaScript
+
+// Exercise 3
+
+/* comments can make code readable,
+easy to reuse and informative */
+
+// Exercise 4
+
+let name = 'Daniel';
+let verdade = true;
+let indefinido;
+let nulo = null;
+
+// Exercise 5
+
+console.log(typeof 'Daniel');
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof 5);
+console.log(typeof null);
+
+// Exercise 6
+
+let indefinido1;
+let indefinido2;
+let indefinido3;
+let indefinido4;
+
+
+// Exercise 7
+
+let name = 'Passos';
+let idade = 20;
+let programmer = true;
+let cat = 'Maju';
+
+// Exercise 8
+
