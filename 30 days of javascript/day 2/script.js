107aff50b93dd6ae8fab571a1cfd291e7bb7ae51
Daniel Passos <laurodanielpassos@gmail.com>
2020-12-27 21:54:51 -0300

    Exercise level 1 done

---
+++ 30 days of javascript/day 2/script.js
@@ -0,0 +1,79 @@
+// Exercise Level 1
+
+//1
+let challenge = '30 Days Of JavaScript';
+
+//2
+console.log(challenge);
+
+//3
+console.log(challenge.length);
+
+//4
+console.log(challenge.toUpperCase());
+
+//5
+console.log(challenge.toLowerCase());
+
+//6
+console.log(challenge.substr(3,18));
+
+//7
+console.log(challenge.substring(0,2));
+
+//8
+console.log(challenge.includes('Script'));
+
+//9 and 10
+console.log(challenge.split(' '));
+
+//11
+let enterprises = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+console.log(enterprises.split(', '));
+
+//12
+console.log(challenge.replace('JavaScript', 'Python'));
+
+//13
+console.log(challenge.charAt(15));
+
+//14
+console.log(challenge.charCodeAt('J'));
+
+//15
+console.log(challenge.indexOf('30 Days Of JavaScript'));
+
+//16
+console.log(challenge.lastIndexOf('30 Days Of JavaScript'));
+
+//17
+let occ = 'You cannot end a sentence with because because because is a conjunction';
+console.log(occ.indexOf('because'));
+
+//18
+console.log(occ.lastIndexOf('because'));
+
+//19
+console.log(occ.search('because'));
+
+//20
+let newChallenge = ' 30 Days Of JavaScript ';
+console.log(newChallenge.trim(' '));
+
+//21
+console.log(challenge.startsWith('30'));
+
+//22
+console.log(challenge.endsWith('Javascript'));
+
+//23
+console.log(challenge.match('a'));
+
+//24
+let c1 = '30 Days of'
+console.log(c1.concat('Javascript'));
+
+//25
+console.log(challenge.repeat(2));
+
+//Exercise level 2
