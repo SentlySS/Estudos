9c7a79f86e965831d04f53c91a0c0673db565094
Daniel Passos <laurodanielpassos@gmail.com>
2021-01-06 23:53:12 -0300

    exercise level 1

--- 30 days of javascript/day 9 High Order Functions/script.js
+++ 30 days of javascript/day 9 High Order Functions/script.js
@@ -5,7 +5,7 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 const products = [
   { product: 'banana', price: 3 },
   { product: 'mango', price: 6 },
-  { product: 'potato', price: ' ' },
+  { product: 'potato', price: '' },
   { product: 'avocado', price: 8 },
   { product: 'coffee', price: 10 },
   { product: 'tea', price: '' },
@@ -19,3 +19,23 @@ const products = [
 // Reduce é para iterar sobre um array e utilizar o valor de cada item para criar um objeto final com base em alguma regra.
 
 // 2. Use forEach to console.log each country in the countries array.
+countries.forEach((pais) => console.log(pais));
+
+// 3. Use forEach to console.log each name in the names array.
+names.forEach((name) => console.log(name));
+
+// 4. Use map to create a new array by changing each country to uppercase in the countries array.
+const countriesUP = countries.map(pais => pais.toUpperCase());
+
+// 5. Use map to create an array of countries length from countries array.
+const countriesLength = countries.map(pais => pais.length);
+
+// 6. Use filter to filter out countries containing land.
+const countriesLand = countries.filter( pais => pais.includes('land'));
+
+// 7. Use filter to filter out countries having six character.
+const countriesLSix = countries.filter(pais => pais.length == 6);
+
+// 8. Use filter to filter out only prices with values.
+const priceWithValue = products.filter(prices => prices.price.valueOf(Number));
+console.log(priceWithValue);
