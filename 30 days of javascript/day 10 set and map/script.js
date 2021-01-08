3a4eb17a3666d0b83f88831153c79199714329c1
Daniel Passos <laurodanielpassos@gmail.com>
2021-01-07 22:09:12 -0300

    exercise 1

---
+++ 30 days of javascript/day 10 set and map/script.js
@@ -0,0 +1,25 @@
+// Exercise level 1
+
+// 1. create an empty set
+const emptySet = new Set();
+
+// 2. Create a set containing 0 to 10 using loop
+for(i=0 ; i < 10; i++){
+  emptySet.add(i);
+}
+
+// 3. Remove an element from a set
+emptySet.delete(2);
+
+// 4. Clear a set
+empty.clear();
+
+// 5. Create a set of 5 string elements from array
+
+const five = ['aracaju', 'sp', 'rj', 'barra','lagarto'];
+const strings = new Set(five);
+
+// 6. Create a map of countries and number of characters of a country
+const countries = [['Brasil', 6], ['Argentina', 9], ['França', 6]];
+const countriesLength = new Map(countries);
+
