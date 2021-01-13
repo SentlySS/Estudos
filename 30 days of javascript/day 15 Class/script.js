4eb8a3aa340cf0d79cbd218fe8e0130ffd62fb31
Daniel Passos <laurodanielpassos@gmail.com>
2021-01-12 22:38:20 -0300

    update exercise

---
+++ 30 days of javascript/day 15 Class/script.js
@@ -0,0 +1,30 @@
+// Exercises
+
+// 1. Create an Animal class. The class will have name, age, color, legs properties and create different methods
+
+class Animal {
+  constructor(name, age, color, legs){
+    this.name = name;
+    this.age = age;
+    this.color = color;
+    this.legs = legs;
+  }
+  eatingMethod(){
+    console.log(`${this.name} is eating`);
+  }
+}
+
+// 2. Create a Dog and Cat child class from the Animal Class.
+
+class Dog extends Animal{
+  latir(){
+    console.log(`${this.name} está latindo`);
+  }
+}
+
+class Cat extends Animal{
+  miar(){
+    console.log(`${this.name} está miando`);
+  }
+}
+
