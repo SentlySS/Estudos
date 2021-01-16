a1d8d20e22847c936fd201021f390d8fe172fed4
Daniel Passos <laurodanielpassos@gmail.com>
2021-01-15 22:29:48 -0300

    day 17 finished

--- 30 days of javascript/day 17 localStorage/script.js
+++ 30 days of javascript/day 17 localStorage/script.js
@@ -10,5 +10,18 @@ const myData = {
 }
 
 const data = JSON.stringify(myData);
+localStorage.setItem('myData', data);
 
-const newData = localStorage.setItem('myData', data);
+// 2. Create a student object. The student object will have first name, last name, age, skills, country, enrolled keys and values for the keys. Store the student object in your browser localStorage.
+
+const student = {
+  firstName: 'Daniel',
+  lastName: 'Passos',
+  age: 20,
+  country: 'Brazil',
+  skill: ['Html', 'css', 'Js'],
+  city: 'Aracaju'
+}
+
+const newstudent = JSON.stringify(student, undefined, 3)
+localStorage.setItem('student', newstudent);
