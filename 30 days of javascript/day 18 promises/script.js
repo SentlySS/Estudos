7054c920eb074c5b9ddb38e0470f65abab6bc43b
Daniel Passos <laurodanielpassos@gmail.com>
2021-01-17 23:21:20 -0300

    day 18.2

--- 30 days of javascript/day 18 promises/script.js
+++ 30 days of javascript/day 18 promises/script.js
@@ -1,1 +1,24 @@
 // Exercise
+
+// 1. Read the countries API using fetch and print the name of country, capital, languages, population and area.
+const url = 'https://restcountries.eu/rest/v2/all'
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+  })
+  .catch(error => console.log(error))
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url)
+      const countries = await response.json()
+      console.log(countries.country)
+      console.log(countries.capital)
+      console.log(countries.languages)
+      console.log(countries.population)
+      console.log(countries.area)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  fetchData()
