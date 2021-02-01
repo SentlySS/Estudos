09c094941ebc9ee119f1b9f4e42257fda514bd92
Daniel Passos <laurodanielpassos@gmail.com>
2021-01-31 23:37:15 -0300

    arrumando o projeto para implementação

---
+++ 30 days of javascript/day 24 project solar system/script.js
@@ -0,0 +1,35 @@
+let planet = document.getElementById('select');
+let value = planet.options[planet.selectedIndex].value;
+
+function calculateMass(){
+  if(value === 1){
+    earth = 1,00
+  }
+  if(value === 2){
+    pluto 0,06
+  }
+  if(value === 3){
+    neptune 1,19
+  }
+  if(value === 4){
+    mars = 0,38
+  }
+  if(value === 5){
+    urane = 0,92
+  }
+  if(value === 6){
+    sapturne = 1,06
+  }
+  if(value === 7){
+    mercury = 0,38
+  }
+  if(value === 8){
+    venus = 0,91
+  }
+  if(value === 9){
+    moon = 
+  }
+  if(value === 10){
+    jupiter = 2,34
+  }
+}
