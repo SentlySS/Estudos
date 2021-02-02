3c8dfc67d9facab3199dc92fb1896c7f5cf43e7a
Daniel Passos <laurodanielpassos@gmail.com>
2021-02-02 16:29:17 -0300

    script final

--- 30 days of javascript/day 24 project solar system/script.js
+++ 30 days of javascript/day 24 project solar system/script.js
@@ -1,16 +1,61 @@
-let planet = document.getElementById('select');
-let value = planet.options[planet.selectedIndex].value;
-
-function calculateMass(){
-    const Planet = {
-      earth: 1 ,
-      pluto: 0.06 ,
-      neptune: 1.19,
-      mars:0.38 ,
-      sapturne: 1.06,
-      mercury: 0.38,
-      venus: 0.91,
-      moon: 0.38,
-      jupiter:2.34
+const space = {
+    mercury:{
+        gravity:0.38,
+        image:"mercury.png"
+    },
+    venus:{
+        gravity:0.91,
+        image:"venus.png"
+    },
+    earth:{
+        gravity:1.0,
+        image:"earth.png"
+    },
+    mars:{
+        gravity:0.38,
+        image:"mars.png"
+    },
+    jupiter:{
+        gravity:2.34,
+        image:"jupiter.png"
+    },
+    saturn:{
+        gravity:0.93,
+        image:"saturn.png"
+    },
+    uranus:{
+        gravity:0.92,
+        image:"uranus.png"
+    },
+    neptune:{
+        gravity:1.12,
+        image:"neptune.png"
     }
 }
+
+const input = document.querySelector("input");
+const select = document.querySelector("select");
+const button = document.querySelector("button")
+const container = document.querySelector(".container");
+const ativar = document.querySelector('.flex-container')
+let weight;
+
+const containerUI = (img,text) => {
+    let image = document.createElement("img");
+    image.src = `images/${img}`;
+    let p = document.createElement("p");
+    p.textContent = text;
+    p.className = "big"
+    container
+    container.appendChild(image);
+    container.appendChild(p);
+}
+button.addEventListener("click",function(){
+    let imgSrc;
+    container.innerHTML = "";
+    imgSrc = space[select.value].image;
+    weight = Number(input.value) * space[select.value].gravity;
+    containerUI(imgSrc,weight);
+    console.log(weight)
+    ativar.classList.add('ativo');
+})
