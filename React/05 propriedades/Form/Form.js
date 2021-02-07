5a211234c570988e3facc72d87bafaaa276fe708
Daniel Passos <laurodanielpassos@gmail.com>
2021-02-06 21:18:43 -0300

    aprendendo propriedades

---
+++ React/05 propriedades/Form/Form.js
@@ -0,0 +1,14 @@
+import Button from './Button';
+import Input from './Input';
+
+const Form = () => {
+  return (
+    <form>
+      <Input id="email" required />
+      <Input id="password" type="password" />
+      <Button />
+    </form>
+  );
+};
+
+export default Form;
