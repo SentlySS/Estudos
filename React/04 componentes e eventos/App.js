fd5acb6eabfd2d9c812d3f88ae59a25d6ffa972e
Daniel Passos <laurodanielpassos@gmail.com>
2021-02-06 21:17:13 -0300

    Aprendendo componentes

---
+++ React/04 componentes e eventos/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <p>Esse é o meu aplicativo</p>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
