6856ff6befe688b6ce26d31dd5f06dde20570243
Daniel Passos <laurodanielpassos@gmail.com>
2021-03-03 19:16:12 -0300

    Creating an reative input

---
+++ React/12 Criando inputs reativos/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Input from "./Form/Input";
+import Select from "./Form/Select";
+
+const App = () => {
+  const [nome, setNome] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [produto, setProduto] = React.useState('');
+
+  return(
+    <form>
+      <Select
+        options={['smartphone', 'tablet']}
+        value={produto}
+        setValue={setProduto}
+      />
+      <Input
+        id="nome"
+        label="Nome"
+        value={nome}
+        setValue={setNome}
+        required
+      />
+
+      <Input
+        id="email"
+        label="Email"
+        value={email}
+        setValue={setEmail}
+        required
+      />
+      <button>Enviar</button>
+    </form>
+  )
+};
+
+export default App;
