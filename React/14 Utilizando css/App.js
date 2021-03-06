fb2fc7687131946d8eaf2971be7b1a99d5826889
Daniel Passos <laurodanielpassos@gmail.com>
2021-03-06 19:49:22 -0300

    Adding css

---
+++ React/14 Utilizando css/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./app.css"
+import Produto from './Components/Produto'
+const App = () => {
+
+  return(
+    <div >
+      <Produto />
+    </div>
+  )
+};
+
+export default App;
