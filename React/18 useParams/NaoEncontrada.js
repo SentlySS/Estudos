a0048c7e1bf217215742a4d88081f2430cffc816
Daniel Passos <laurodanielpassos@gmail.com>
2021-03-12 12:20:00 -0300

    testing useParams

---
+++ React/18 useParams/NaoEncontrada.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NaoEncontrada = () => {
+  return (
+    <div>
+      <p>Error: 404 - página não encontrada</p>
+    </div>
+  )
+}
+
+export default NaoEncontrada;
