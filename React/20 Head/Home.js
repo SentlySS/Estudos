0922a933c3c78d927168ae19c4e61ae40a2dbe4a
Daniel Passos <laurodanielpassos@gmail.com>
2021-03-12 12:57:25 -0300

    Creating tag Head

---
+++ React/20 Head/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Head from './Head';
+
+const Home = () => {
+  return (
+    <div>
+      <Head title="Home" description="Descrição da home aqui"/>
+      <h1>Home</h1>
+      <p>Essa é a home</p>
+    </div>
+  )
+}
+
+export default Home;
