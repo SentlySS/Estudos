78527776473ae0ed374e75ed860e595c4bf2b61c
Daniel Passos <laurodanielpassos@gmail.com>
2021-03-14 18:02:13 -0300

    Criando a página de contatos

---
+++ React/21 Criando um projeto com router/Components/Contato.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styles from './Contato.module.css';
+import foto from '../img/contato.jpg'
+import Head from './Head';
+const Contato = () => {
+  return (
+    <section className={styles.contato + " animeLeft"}>
+      <Head title="Ranek | Contato" description="Entre em contato"/>
+      <img src={foto} alt="Máquina de escrever" />
+      <div>
+      <h1> Entre em contato.</h1>
+      <ul className={styles.dados}>
+        <li>laurodanielpassos@gmail.com</li>
+        <li>99999-9999</li>
+        <li>Rua Logo Ali, 999</li>
+      </ul>
+      </div>
+    </section>
+  )
+}
+
+export default Contato
