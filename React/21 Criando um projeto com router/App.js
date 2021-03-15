da77a66de23d2100520432853a59e3c089b6201a
Daniel Passos <laurodanielpassos@gmail.com>
2021-03-15 19:20:18 -0300

    Criando fetch e lidando com as rotas

--- React/21 Criando um projeto com router/App.js
+++ React/21 Criando um projeto com router/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Produtos from "./Components/Produtos";
+import Produto from "./Components/Produto";
 import Contato from "./Components/Contato";
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
         <div className="content">
           <Routes>
             <Route  path="/" element={<Produtos />}/>
+            <Route  path="produto/:id" element={<Produto />}/>
             <Route  path="contato" element={<Contato />}/>
           </Routes>
         </div>
