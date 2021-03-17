f29e532c163bdd69b8778681e11b36e1584c671f
Daniel Passos <laurodanielpassos@gmail.com>
2021-03-17 18:41:47 -0300

    Learning PropTypes

---
+++ React/22 PropTYpes/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from './Button'
+
+const App = () => {
+  return(
+    <div>
+      <Button width={100}>
+        Clique Aqui
+      </Button>
+    </div>
+  )
+};
+
+export default App;
